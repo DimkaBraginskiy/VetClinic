54b0393626b10ab38fa74a75d1f72b68ec63d312
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2026-06-03 21:53:34 +0200

    feat: add Veterinarian logic

--- VetClinic/Domain/Entities/Appointment.cs
+++ VetClinic/Domain/Entities/Appointment.cs
@@ -9,6 +9,7 @@ public abstract class Appointment
     public AppointmentStatus Status { get; private set; }
     public AppointmentType Type { get; private set; }
     public DateTime StartDate { get; private set; }
+    public DateTime EndDate { get; private set; }
     public decimal BasePrice { get; private set; }
     public string? PaymentDetails { get; private set; }
 
@@ -22,5 +23,13 @@ public abstract class Appointment
     public ICollection<Animal> Animals { get; private set; } = new List<Animal>();
     public ICollection<Customer> Customers { get; private set; } = new List<Customer>();
     public ICollection<Discount> Discounts { get; private set; } = new List<Discount>();
-    
+
+    public Appointment() { }
+
+    public Appointment(DateTime startDate, DateTime endDate, Guid veterinarianId)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+        VeterinarianId = veterinarianId;
+    }
 }
--- VetClinic/Domain/Entities/Veterinarian.cs
+++ VetClinic/Domain/Entities/Veterinarian.cs
@@ -4,35 +4,41 @@ namespace VetClinic.Domain.Entities;
 
 public class Veterinarian : Person
 {
+    public Guid Id { get; private set; } = Guid.NewGuid();
     public VeterinarianType Type { get; private set; }
     public decimal Salary { get; private set; }
-    public DateTime EmploymentDate { get; private set; }    
-    
-    public Clinic Clinic { get; private set; }
+    public DateTime EmploymentDate { get; private set; }
+
     public Guid ClinicId { get; private set; }
+    public Clinic Clinic { get; private set; }
     
     private readonly List<Shift> _shifts = new();
     private readonly List<Treatment> _treatments = new();
     private readonly List<Appointment> _appointments = new();
-    
     public IReadOnlyCollection<Shift> Shifts => _shifts.AsReadOnly();
     public IReadOnlyCollection<Treatment> Treatments => _treatments.AsReadOnly();
-    public IReadOnlyCollection<Appointment> Appointments => _appointments.AsReadOnly();
+    public IReadOnlyCollection<Appointment> Appointments => 
+        _appointments.OrderBy(a => a.StartDate).ToList().AsReadOnly();
+    
+    public Veterinarian() { }
 
     public Veterinarian(
         VeterinarianType type,
         decimal salary,
         DateTime employmentDate, 
         List<Treatment> treatments,
+        Clinic clinic,
         string firstName,
         string lastName,
         string email,
         string? middleName = null) : base(firstName, lastName, email, middleName)
     {
-        Validate(type, salary, employmentDate, treatments);
+        Validate(type, salary, employmentDate, treatments, clinic);
         Type = type;
         Salary = salary;
         EmploymentDate = employmentDate;
+        ClinicId = clinic.Id;
+        Clinic = clinic;
 
         foreach (var treatment in treatments)
         {
@@ -40,7 +46,7 @@ public class Veterinarian : Person
         }
     }
 
-    private void Validate(VeterinarianType type, decimal salary, DateTime employmentDate, List<Treatment> treatments)
+    private void Validate(VeterinarianType type, decimal salary, DateTime employmentDate, List<Treatment> treatments, Clinic clinic)
     {
         if (!Enum.IsDefined(type))
         {
@@ -61,6 +67,11 @@ public class Veterinarian : Person
         {
             throw new ArgumentException("A Veterinarian can not have 0 treatments");
         }
+        
+        if(clinic == null)
+        {
+            throw new ArgumentException("Clinic can not be null");
+        }
     }
 
     public void AddTreatment(Treatment treatment)
@@ -68,7 +79,11 @@ public class Veterinarian : Person
         if (treatment.Type is TreatmentType.Surgery or TreatmentType.Chiropractic && Type != VeterinarianType.Surgeon){
             throw new InvalidOperationException("Only a surgeon can perform a surgery");
         }
-        
+        if(Treatments.Any(t => t.Type == treatment.Type))
+        {
+            throw new InvalidOperationException($"Treatment {treatment} already assigned");
+        }
+
         _treatments.Add(treatment);
     }
 
@@ -88,8 +103,7 @@ public class Veterinarian : Person
 
     public void AddShift(Shift shift)
     {
-        var shiftOverlap = Shifts.FirstOrDefault(s => s.StartTime.Day.Equals(shift.StartTime.Day));
-        if (shiftOverlap != null)
+        if (Shifts.Any(s => s.StartTime.Date == shift.StartTime.Date))
         {
             throw new InvalidOperationException("A veterinarian can not have 2 shifts on the same day");
         }
@@ -106,5 +120,55 @@ public class Veterinarian : Person
         }
 
         _shifts.Remove(shift);
+    }
+
+    public void AddAppointment(Appointment appointment)
+    {
+        if (appointment == null)
+        {
+            throw new ArgumentNullException(nameof(appointment));
+        }
+
+        ValidateAppointment(appointment);
+        
+        if (_appointments.Any(a => a.Id == appointment.Id))
+            throw new InvalidOperationException("This appointment is already assigned.");
+        
+        _appointments.Add(appointment);
+    }
+
+    private void ValidateAppointment(Appointment appointment)
+    {
+        var hasShiftDuringDay = _shifts.Any(s => 
+            s.StartTime.Date == appointment.StartDate.Date);
+
+        if (!hasShiftDuringDay)
+        {
+            throw new InvalidOperationException(
+                $"Veterinarian has no shift on {appointment.StartDate.Date:yyyy-m-dd}. Can not assign shift");
+        }
+
+        var hasOverlap = _appointments.Any(a =>
+            a.Status != AppointmentStatus.Cancelled && HasTimeOverlap(a, appointment));
+
+        if (hasOverlap)
+        {
+            throw new InvalidOperationException("Appointment which you want to create overlaps with existing one");
+        }
+    }
+
+    private bool HasTimeOverlap(Appointment existingAppointment, Appointment newAppointment)
+    {
+        return newAppointment.StartDate < existingAppointment.EndDate &&
+               newAppointment.EndDate > existingAppointment.StartDate;
+    }
+    
+    public void RemoveAppointment(Guid appointmentId)
+    {
+        var appt = _appointments.FirstOrDefault(a => a.Id == appointmentId);
+        if (appt == null)
+            throw new ArgumentException("Appointment not found.");
+
+        _appointments.Remove(appt);
     }
 }
